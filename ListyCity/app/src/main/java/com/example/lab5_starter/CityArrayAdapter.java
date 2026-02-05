d154ff26975403c3ab2975d4448168ff5728d5e2
rsheikhdon <rsheikhdon124@gmail.com>
2026-02-05 11:34:41 -0700

    Enhance CityArrayAdapter with delete functionality
    Added functionality to delete cities from both the local list and Firestore.

--- ListyCity/app/src/main/java/com/example/lab5_starter/CityArrayAdapter.java
+++ ListyCity/app/src/main/java/com/example/lab5_starter/CityArrayAdapter.java
@@ -1,28 +1,35 @@
-package com.example.lab5_starter;
+package com.example.lab5starter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.google.firebase.firestore.CollectionReference;
 
 import java.util.ArrayList;
 
 public class CityArrayAdapter extends ArrayAdapter<City> {
     private ArrayList<City> cities;
     private Context context;
+    private CollectionReference citiesRef;
 
-    public CityArrayAdapter(Context context, ArrayList<City> cities){
+    public CityArrayAdapter(Context context, ArrayList<City> cities, CollectionReference citiesRef){
         super(context, 0, cities);
         this.cities = cities;
         this.context = context;
+        this.citiesRef = citiesRef;
     }
 
     @NonNull
+    @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
         View view = convertView;
         if (view == null){
@@ -30,11 +37,25 @@ public class CityArrayAdapter extends ArrayAdapter<City> {
         }
 
         City city = cities.get(position);
-        TextView movieName = view.findViewById(R.id.textCityName);
-        TextView movieYear = view.findViewById(R.id.textCityProvince);
+        TextView cityName = view.findViewById(R.id.textCityName);
+        TextView cityProvince = view.findViewById(R.id.textCityProvince);
+        Button deleteButton = view.findViewById(R.id.buttonDeleteCity);
 
-        movieName.setText(city.getName());
-        movieYear.setText(city.getProvince());
+        cityName.setText(city.getName());
+        cityProvince.setText(city.getProvince());
+
+        // Deletes a city
+        deleteButton.setOnClickListener(v -> {
+            // Removes that deleted city from a local list of cities
+            cities.remove(position);
+            notifyDataSetChanged();
+
+            // Removes the city from Firestore
+            citiesRef.document(city.getName())
+                    .delete()
+                    .addOnSuccessListener(aVoid -> Log.d("Firestore", "City successfully deleted!"))
+                    .addOnFailureListener(e -> Log.e("Firestore", "Error deleting city", e));
+        });
 
         return view;
     }
